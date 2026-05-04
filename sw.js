976fa1a0d84b0932146d4a4aa294f0ceff2b0bb6
Kreayotoo <claudekreayotoo@gmail.com>
2026-05-04 16:10:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Kreayotoo Workforce Portal - Service Worker
-const CACHE_VERSION = 'v53-2026-05-04-real-kreayotoo-logo';
+const CACHE_VERSION = 'v54-2026-05-04-new-make-in-india-logo';
 const CACHE_NAME = `kreayotoo-portal-${CACHE_VERSION}`;
 
 const ASSETS = [
